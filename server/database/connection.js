453aa2bb406cd6f198656d52aaf00387877aed91
Nishant Sharma <nishant-ai@outlook.com>
2023-02-13 19:51:09 +0530

    Shifted to MySQL

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,23 +1,5 @@
-const dbKeys = require("./keys");
-
-const { Pool } = require("pg");
-const { Sequelize } = require('sequelize');
-
-const pgClient = new Pool({
-  user: dbKeys.pgUser,
-  host: dbKeys.pgHost,
-  database: dbKeys.pgDatabase,
-  password: dbKeys.pgPassword,
-  port: dbKeys.pgPort,
-});
-
-// On Connection
-pgClient.on("connect", (client) => {
-  console.log("Postgres Connected");
-});
+const { Sequelize } = require("sequelize");
 
 // connect db with sequelize
-
 const sequelize = new Sequelize();
-
 module.exports = { sequelize };
