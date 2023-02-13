6e9e3a6c9e0b35807667e68808d5a22d058ba8d2
Palash <palashnath880@gmail.com>
2023-02-13 23:14:51 +0600

    Issue Solved

--- server/models/batchModel.js
+++ server/models/batchModel.js
@@ -1,34 +1,32 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../database/connection');
 
-const batchModel = sequelize.define('batch', {
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-    },
-    org_id: {
-        type: DataTypes.UUID,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notNull: { msg: 'name is required' }
-        }
-    },
-    reg_date: DataTypes.DATE,
-    active: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        validate: {
-            notNull: { msg: 'required' }
-        }
-    },
-    end_date: DataTypes.DATE,
-    classroom_link: DataTypes.STRING,
-    average_band: DataTypes.INTEGER,
-    total_average_band: DataTypes.INTEGER,
-});
-
-module.exports = batchModel;
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('batch', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        org_id: {
+            type: DataTypes.UUID,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: { msg: 'name is required' }
+            }
+        },
+        reg_date: DataTypes.DATE,
+        active: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            validate: {
+                notNull: { msg: 'required' }
+            }
+        },
+        end_date: DataTypes.DATE,
+        classroom_link: DataTypes.STRING,
+        average_band: DataTypes.INTEGER,
+        total_average_band: DataTypes.INTEGER,
+    });
+};
