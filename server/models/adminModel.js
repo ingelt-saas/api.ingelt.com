1e32c985a1dcca88b3ead2e9cf14de4692373fa8
Nishant Sharma <nishant-ai@outlook.com>
2023-02-15 12:47:05 +0530

    Merged Nand's Work

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -1,36 +1,41 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('admin', {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-        },
-        org_id: {
-            type: DataTypes.UUID,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notNull: { msg: 'name is required' }
-            }
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notNull: { msg: 'email is required' }
-            }
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notNull: { msg: 'password is required' }
-            }
-        },
-        picture: {
-            type: DataTypes.STRING,
-        }
-    });
+  return sequelize.define("admin", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+
+    org_id: {
+      type: DataTypes.UUID,
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "name is required" },
+      },
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "email is required" },
+      },
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "password is required" },
+      },
+    },
+
+    picture: {
+      type: DataTypes.STRING,
+    },
+  });
 };
--- server/models/documentModel.js
+++ server/models/documentModel.js
@@ -1,24 +1,27 @@
-
 const documentModel = (sequelize, DataTypes) => {
-    return sequelize.define('document', {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-        },
-        org_id: {
-            type: DataTypes.UUID,
-        },
-        file: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notNull: { msg: 'file is required' }
-            }
-        },
-        filesize: DataTypes.STRING,
-        date: DataTypes.DATE,
-    });
-}
+  return sequelize.define("document", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+
+    org_id: {
+      type: DataTypes.UUID,
+    },
+
+    file: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "file is required" },
+      },
+    },
+
+    filesize: DataTypes.STRING,
+
+    date: DataTypes.DATE,
+  });
+};
 
 module.exports = documentModel;
