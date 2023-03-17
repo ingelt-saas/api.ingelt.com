03e5afc009be9728a417387c30e84fcc96ef3956
Palash <palashnath880@gmail.com>
2023-03-18 00:05:22 +0600

    Route Updated

--- services/teacher/index.js
+++ services/teacher/index.js
@@ -3,24 +3,40 @@ const router = express.Router();
 
 // Teacher Service Import
 const homeService = require('./home');
-const studentService = require('./student');
-const teacherService = require('./teacher');
-const batchService = require('./batch');
+const documentService = require('./document');
+const discussionService = require('./discussion');
+const assignmentService = require('./assignment');
 const mockTestService = require('./mockTest');
+const notesService = require('./notes');
+const settingService = require('./setting');
+const studentService = require('./student');
+const submissionService = require('./submission');
 
 // Home Route 
 router.use('/', homeService);
 
-// Student Route
-router.use('/student', studentService);
+// Document Route 
+router.use('/document', documentService);
 
-// Teacher Route 
-router.use('/teacher', teacherService);
+// Discussion Route 
+router.use('/discussion', discussionService);
 
-// Batch Route 
-router.use('/batch', batchService);
+// Assignment Route 
+router.use('/assignment', assignmentService);
 
 // Mock Test Route 
 router.use('/mockTest', mockTestService);
+
+// Notes Route 
+router.use('/notes', notesService);
+
+// Setting Route 
+router.use('/setting', settingService);
+
+// Student Route 
+router.use('/student', studentService);
+
+// Submission Route 
+router.use('/submission', submissionService);
 
 module.exports = router;
