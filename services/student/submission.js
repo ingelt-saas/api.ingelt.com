076661026c146fd3712a5a5859b3cf3e5a226f95
Palash <palashnath880@gmail.com>
2023-03-28 00:30:27 +0600

    File Upload Service Added

--- services/student/submission.js
+++ services/student/submission.js
@@ -2,17 +2,22 @@ const express = require("express");
 const submissionService = express.Router();
 const submissionUtil = require("../../utils/submission");
 const uploadService = require('../../uploads/index');
+const deleteFile = require("../../uploads/deleteFile");
 
 // create submission
 submissionService.post("/", uploadService('submissions').single('file'), async (req, res) => {
   try {
+
+    const student = req.headers.authorization.split(" ")[1];
+    const studentId = jwt.decode(student).id;
+
     const file = req.file;
     const newSubmission = {
       assignmentId: req.body.assignmentId,
-      studentId: req.body.studentId,
+      studentId: studentId,
       file: file.filename,
     };
-    const result = await submissionUtil.create(req.body);
+    const result = await submissionUtil.create(newSubmission);
     res.status(201).json(result);
   } catch (err) {
     res.status(400).send(err);
@@ -20,7 +25,7 @@ submissionService.post("/", uploadService('submissions').single('file'), async (
 });
 
 // get submission by student
-submissionService.get("/get", async (req, res) => {
+submissionService.get("/", async (req, res) => {
   try {
     const student = req.headers.authorization.split(" ")[1];
     const studentId = jwt.decode(student).id;
@@ -31,5 +36,31 @@ submissionService.get("/get", async (req, res) => {
     res.status(400).send(err);
   }
 });
+
+// submission update
+submissionService.put('/:submissionId', async (req, res) => {
+  try {
+    const result = await submissionUtil.update(req.params.submissionId, req.body);
+    res.send(result);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// submission delete
+submissionService.delete('/:submissionId', async (req, res) => {
+  try {
+    // get the file
+    const findSubmission = await submissionUtil.readById(req.params.submissionId);
+    // delete file
+    await deleteFile(`uploads/submissions/${findSubmission?.file}`);
+    // delete data from db
+    const result = await submissionUtil.delete(req.params.submissionId);
+    res.json(result);
+
+  } catch (err) {
+    res.status(400).send(err);
+  }
+})
 
 module.exports = submissionService;
