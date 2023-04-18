d76aad61ffee573b2d534bc2d663124fc27266f3
Nishant Sharma <nishant-ai@outlook.com>
2023-04-18 13:47:32 +0530

    Minute Error Resolution

--- services/student/submission.js
+++ services/student/submission.js
@@ -5,7 +5,6 @@ const submissionUtil = require("../../utils/submission");
 // create submission
 submissionService.post("/", async (req, res) => {
   try {
-
     const student = req.headers.authorization.split(" ")[1];
     const studentId = jwt.decode(student).id;
 
@@ -36,9 +35,12 @@ submissionService.get("/", async (req, res) => {
 });
 
 // submission update
-submissionService.put('/:submissionId', async (req, res) => {
+submissionService.put("/:submissionId", async (req, res) => {
   try {
-    const result = await submissionUtil.update(req.params.submissionId, req.body);
+    const result = await submissionUtil.update(
+      req.params.submissionId,
+      req.body
+    );
     res.send(result);
   } catch (err) {
     res.status(400).send(err);
@@ -46,19 +48,21 @@ submissionService.put('/:submissionId', async (req, res) => {
 });
 
 // submission delete
-submissionService.delete('/:submissionId', async (req, res) => {
+submissionService.delete("/:submissionId", async (req, res) => {
   try {
     // get the file
-    const findSubmission = await submissionUtil.readById(req.params.submissionId);
+    const findSubmission = await submissionUtil.readById(
+      req.params.submissionId
+    );
     // delete file
-    findSubmission?.file && await deleteFile(`uploads/submissions/${findSubmission?.file}`);
+    findSubmission?.file &&
+      (await deleteFile(`uploads/submissions/${findSubmission.file}`));
     // delete data from db
     const result = await submissionUtil.delete(req.params.submissionId);
     res.json(result);
-
   } catch (err) {
     res.status(400).send(err);
   }
-})
+});
 
 module.exports = submissionService;
