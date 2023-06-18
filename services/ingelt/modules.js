b5b2b6bc0a30e32a4e5ecc2973d7ff786db4e70a
Palash <palashnath880@gmail.com>
2023-06-18 16:20:20 +0600

    Module Update and Delete Route Updated

--- services/ingelt/modules.js
+++ services/ingelt/modules.js
@@ -10,7 +10,9 @@ const awsUpload = require('../../aws/upload');
 // create modules
 moduleService.post('/', upload.fields([{ name: 'file' }, { name: 'thumbnail' }]), async (req, res) => {
     try {
-        const moduleVideo = req.files.file[0];
+
+        const newModule = req.body;
+        const moduleVideo = req.files.file ? req.files.file[0] : null;
         const thumbnail = req.files.thumbnail[0];
 
         const fileUpload = (file, filePath) => new Promise((resolve, reject) => {
@@ -23,17 +25,19 @@ moduleService.post('/', upload.fields([{ name: 'file' }, { name: 'thumbnail' }])
             });
         });
 
-        const uploadedModule = await fileUpload(moduleVideo, 'ingelt/modules/videos');
-        const uploadedThumbnail = await fileUpload(thumbnail, 'ingetl/modules/thumbnails');
+        if (moduleVideo) {
+            const uploadedModule = await fileUpload(moduleVideo, 'ingelt/modules/videos');
+            newModule.file = uploadedModule.Key;
+            newModule.fileSize = moduleVideo.size;
+        }
 
-        const newModule = req.body;
-        newModule.name = moduleVideo.originalname;
-        newModule.file = uploadedModule.Key;
-        newModule.fileSize = moduleVideo.size;
+        const uploadedThumbnail = await fileUpload(thumbnail, 'ingelt/modules/thumbnails');
         newModule.thumbnail = uploadedThumbnail.Key;
+
         const result = await moduleUtil.create(newModule);
         res.status(201).json(result);
     } catch (err) {
+        console.log(err)
         res.status(400).send(err);
     }
 });
@@ -44,6 +48,56 @@ moduleService.get('/getall', async (req, res) => {
         const { s, pageNo, limit, subject } = req.query;
         const result = await moduleUtil.getModules(parseInt(pageNo), parseInt(limit), subject, s);
         res.json(result);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+// read by id
+moduleService.get('/:moduleId', async (req, res) => {
+    try {
+        const result = await moduleUtil.readById(req.params.moduleId);
+        res.json(result);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+// update modules 
+moduleService.put('/:moduleId', upload.fields([{ name: 'file' }, { name: 'thumbnail' }]), async (req, res) => {
+    try {
+
+        const updateData = req.body;
+        const getModule = await moduleUtil.readById();
+
+        const moduleVideo = req.files.file ? req.files.file[0] : null;
+        const thumbnail = req.files.thumbnail ? req.files.thumbnail[0] : null;
+
+        const fileUpload = (file, filePath) => new Promise((resolve, reject) => {
+            awsUpload(file, filePath, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data)
+                }
+            });
+        });
+
+        if (moduleVideo) {
+            const uploadedModule = await fileUpload(moduleVideo, 'ingelt/modules/videos');
+            updateData.file = uploadedModule.Key;
+            updateData.fileSize = moduleVideo.size;
+            getModule.file && await deleteFile(getModule.file);
+        }
+
+        if (thumbnail) {
+            const uploadedThumbnail = await fileUpload(thumbnail, 'ingelt/modules/thumbnails');
+            updateData.thumbnail = uploadedThumbnail.Key;
+            getModule.thumbnail && await deleteFile(getModule.thumbnail);
+        }
+
+        const result = await moduleUtil.update(req.params.moduleId, updateData);
+        res.status(200).json(result);
     } catch (err) {
         res.status(400).send(err);
     }
