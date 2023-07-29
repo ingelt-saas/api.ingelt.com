12e089e888fb1f54c634c5f6aaef743fe35df326
Palash <palashnath880@gmail.com>
2023-07-29 18:43:50 +0600

    Updated

---
+++ razorpay/razorpay.js
@@ -0,0 +1,29 @@
+const Razorpay = require('razorpay');
+
+
+// create a instance
+const instance = new Razorpay({
+    key_id: process.env.RAZOR_KEY_ID,
+    key_secret: process.env.RAZOR_KEY_SECRET
+});
+
+
+exports.createOrder = ({ amount }) => new Promise((resolve, reject) => {
+    try {
+
+        // create a order
+        instance.orders.create({
+            amount: amount,
+            currency: 'INR',
+        }, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+
+    } catch (err) {
+        reject(err);
+    }
+});
