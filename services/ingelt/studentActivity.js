1753e2b9989cd006c0e89cbf2aa31523dbbbc584
Palash <palashnath880@gmail.com>
2023-08-23 14:13:21 +0600

    Updated

--- services/ingelt/studentActivity.js
+++ services/ingelt/studentActivity.js
@@ -3,11 +3,32 @@ const studentActivityUtil = require('../../utils/studentActivity');
 const studentActivityService = require('express').Router();
 
 // getall active students
-studentActivityService.get('/getActiveStudents', async (req, res) => {
+studentActivityService.get('/getStudentsActivity', async (req, res) => {
     try {
         const { year, month } = req.query;
-        const result = await studentActivityUtil.getActiveStudents(year, month);
-        console.log(result);
+        const result = await studentActivityUtil.getStudentActivity(year, month);
+        res.json(result);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+// get students by date
+studentActivityService.get('/getStudentsByDate', async (req, res) => {
+    try {
+        const { date } = req.query;
+        const result = await studentActivityUtil.getStudentsByDate(date);
+        res.json(result);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+//  get activities by student id and year and month
+studentActivityService.get('/getActivities/:studentId', async (req, res) => {
+    try {
+        const { year, month } = req.query;
+        const result = await studentActivityUtil.getActivitiesByStudent(req.params.studentId, year, month);
         res.json(result);
     } catch (err) {
         res.status(400).send(err);
--- utils/studentActivity.js
+++ utils/studentActivity.js
@@ -4,19 +4,108 @@ const { studentActivity, student, sequelize, Sequelize } = require("../models");
 const studentActivityUtil = {};
 
 // get all active students
-studentActivityUtil.getActiveStudents = async (year, month) => {
+studentActivityUtil.getStudentActivity = async (year, month) => {
     try {
         const result = await studentActivity.findAll({
             attributes: [
                 [sequelize.fn('date', sequelize.col('createdAt')), 'date'], // Extract the date part from createdAt
                 [sequelize.fn('COUNT', sequelize.fn('DISTINCT', sequelize.col('studentId'))), 'studentCount'], // Count the students
             ],
-            where: sequelize.literal(` YEAR(createdAt) = ${year} AND MONTH(createdAt) = ${month} AND active = 1`),
+            where: sequelize.literal(` YEAR(createdAt) = ${year} AND MONTH(createdAt) = ${month}`),
             group: [sequelize.fn('date', sequelize.col('createdAt'))], // Group by date
 
         });
         return result;
     } catch (err) {
+        throw err;
+    }
+}
+
+// get spend hour by date
+studentActivityUtil.getSpendHours = async (date) => {
+    try {
+        const result = await studentActivity.findAll({
+            attributes: [
+                'studentId',
+                [Sequelize.fn('TIMESTAMPDIFF', Sequelize.literal('HOUR'), Sequelize.col('createdAt'), Sequelize.col('endDate')), 'hoursDifference'],
+            ],
+            group: 'studentId',
+        });
+
+        const totalHoursByStudent = {};
+        result.forEach(row => {
+            const { studentId, hoursDifference } = row.dataValues;
+            if (!totalHoursByStudent[studentId]) {
+                totalHoursByStudent[studentId] = 0;
+            }
+            totalHoursByStudent[studentId] += hoursDifference;
+        });
+    } catch (err) {
+        throw err;
+    }
+}
+
+// get students by id
+studentActivityUtil.getStudentsByDate = async (date) => {
+    try {
+        date = new Date(date);
+        const date2 = new Date(date);
+        date2.setDate(date2.getDate() + 1);
+
+        const result = await studentActivity.findAll({
+            where: {
+                createdAt: {
+                    [Op.between]: [date, date2],
+                }
+            },
+            attributes: ['studentId'],
+            group: 'studentId',
+        });
+
+        const studentIds = result.map(i => i.dataValues.studentId && i.dataValues.studentId);
+
+        const students = await student.findAll({
+            where: {
+                id: studentIds,
+            },
+            attributes: ['id', 'name', 'image', 'email', 'gender']
+        });
+
+        return students;
+    } catch (err) {
+        throw err;
+    }
+}
+
+// get activities by student and year and month
+studentActivityUtil.getActivitiesByStudent = async (studentId, year, month) => {
+    try {
+        const result = await studentActivity.findAll({
+            where: {
+                studentId: studentId,
+                [Op.and]: [
+                    Sequelize.where(Sequelize.fn('YEAR', Sequelize.col('createdAt')), year),
+                    Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('createdAt')), month)
+                ]
+            },
+            attributes: [
+                'studentId',
+                [Sequelize.fn('DATE', Sequelize.col('createdAt')), 'date'],
+                // [Sequelize.fn('TIMESTAMPDIFF', Sequelize.literal('SECONDS'), Sequelize.col('createdAt'), Sequelize.col('endDate')), 'seconds'],
+                [Sequelize.literal('SUM(TIMESTAMPDIFF(SECOND, createdAt, endDate))'), 'totalSeconds']
+            ],
+            group: ['date']
+        });
+
+        const getStudent = await student.findOne({
+            attributes: ['name', 'id', 'email', 'image', 'gender'],
+            where: { id: studentId }
+        });
+
+        return { activities: result, student: getStudent };
+
+    } catch (err) {
+        console.log(err);
         throw err;
     }
 }
